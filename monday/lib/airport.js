d97a63a7f15f21f8a7a39e7d95aa8fc242e09e9c
Rob Baile <robbaile12@outlook.com>
2019-11-11 11:09:57 +0000

    added test for airport class

--- monday/lib/airport.js
+++ monday/lib/airport.js
@@ -3,6 +3,14 @@ class Airport {
         this.hangar = [];
         this.capacity = 20;
     }
+
+    landPlane(plane) {
+        if(this.hangar.length == this.capacity) {
+            throw new Error("The hangar is full!")
+        };
+
+        this.hangar.push(plane)
+    }
 }
 
-module.exports = Airport
+module.exports = Airport;
--- monday/spec/airportSpec.js
+++ monday/spec/airportSpec.js
@@ -1,4 +1,4 @@
-describe("when song has been paused", function() {
+describe("Airport", function() {
     let Airport = require('../lib/airport');
     var airport;
     
@@ -6,7 +6,28 @@ describe("when song has been paused", function() {
       airport = new Airport();
     });
 
-    it("should have an empty hangar array", function() {
+    it("should have an empty hangar array", () => {
         expect(airport.hangar).toEqual([]);  
+    });
+
+    it("should be able to land a plane and add it to the hangar", () => {
+      plane = {flightNumber: "01234"}
+      airport.landPlane(plane);
+
+      expect(airport.hangar.length).toEqual(1);
+    });
+
+    it("should not let more than capacity planes land", () => {
+      plane = {flightNumber: "01234"}
+    
+      for (let index = 0; index < 20; index++) {
+        airport.landPlane(plane); 
+      }
+      
+      expect(() => airport.landPlane(plane)).toThrow();
+    });
+
+    it("should let the ready plane take off", () => {
+
     });
 });
